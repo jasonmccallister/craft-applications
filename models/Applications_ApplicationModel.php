4e5e69e1ce81228074eea69c84d8cd56e2fe0c01
Jason McCallister <themccallister@gmail.com>
2014-07-10 11:24:23 -0700

    Add applicationStatus to model

--- models/Applications_ApplicationModel.php
+++ models/Applications_ApplicationModel.php
@@ -30,6 +30,15 @@ class Applications_ApplicationModel extends BaseElementModel
                 'applicantName' => AttributeType::String,
                 'applicantEmail' => AttributeType::Email,
                 'applicantPhone' => AttributeType::String,
+                'applicationStatus' => array(
+                    AttributeType::Enum,
+                    'values' => array(
+                        ApplicationsApplicationStatus::Approved,
+                        ApplicationsApplicationStatus::Denied,
+                        ApplicationsApplicationStatus::Pending
+                    ),
+                    'default' => ApplicationsApplicationStatus::Pending
+                ),
                 'submitDate'  => AttributeType::DateTime,
             )
         );
