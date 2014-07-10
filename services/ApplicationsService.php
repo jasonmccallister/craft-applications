1556f98375ebec90630ac09afe3c272b42496da1
Jason McCallister <themccallister@gmail.com>
2014-07-09 22:23:29 -0700

    Move to spaces

--- services/ApplicationsService.php
+++ services/ApplicationsService.php
@@ -6,117 +6,117 @@ namespace Craft;
  */
 class ApplicationsService extends BaseApplicationComponent
 {
-	/**
-	 * Returns an application by its ID.
-	 *
-	 * @param int $applicationId
-	 * @return Applications_ApplicationModel|null
-	 */
-	public function getApplicationById($applicationId)
-	{
-		return craft()->elements->getElementById($applicationId, 'Applications_Application');
-	}
+    /**
+     * Returns an application by its ID.
+     *
+     * @param int $applicationId
+     * @return Applications_ApplicationModel|null
+     */
+    public function getApplicationById($applicationId)
+    {
+        return craft()->elements->getElementById($applicationId, 'Applications_Application');
+    }
 
-	/**
-	 * Saves an application.
-	 *
-	 * @param Applications_ApplicationModel $event
-	 * @throws Exception
-	 * @return bool
-	 */
-	public function saveApplication(Applications_ApplicationModel $application)
-	{
-		$isNewApplication = !$application->id;
+    /**
+     * Saves an application.
+     *
+     * @param Applications_ApplicationModel $event
+     * @throws Exception
+     * @return bool
+     */
+    public function saveApplication(Applications_ApplicationModel $application)
+    {
+        $isNewApplication = !$application->id;
 
-		// Application data
-		if (!$isNewApplication)
-		{
-			$applicationRecord = Applications_ApplicationRecord::model()->findById($application->id);
+        // Application data
+        if (!$isNewApplication)
+        {
+            $applicationRecord = Applications_ApplicationRecord::model()->findById($application->id);
 
-			if (!$applicationRecord)
-			{
-				throw new Exception(Craft::t('No application exists with the ID “{id}”', array('id' => $application->id)));
-			}
-		}
-		else
-		{
-			$applicationRecord = new Applications_ApplicationRecord();
-		}
+            if (!$applicationRecord)
+            {
+                throw new Exception(Craft::t('No application exists with the ID “{id}”', array('id' => $application->id)));
+            }
+        }
+        else
+        {
+            $applicationRecord = new Applications_ApplicationRecord();
+        }
 
-		$applicationRecord->formId = $application->formId;
-		$applicationRecord->submitDate  = $application->submitDate;
+        $applicationRecord->formId = $application->formId;
+        $applicationRecord->submitDate  = $application->submitDate;
 
-		$applicationRecord->validate();
-		$application->addErrors($applicationRecord->getErrors());
+        $applicationRecord->validate();
+        $application->addErrors($applicationRecord->getErrors());
 
-		if (!$application->hasErrors())
-		{
-			$transaction = craft()->db->getCurrentTransaction() === null ? craft()->db->beginTransaction() : null;
-			try
-			{
-				// Fire an 'onBeforeSaveApplication' event
-				$this->onBeforeSaveApplication(new Event($this, array(
-					'application'      => $application,
-					'isNewApplication' => $isNewApplication
-				)));
+        if (!$application->hasErrors())
+        {
+            $transaction = craft()->db->getCurrentTransaction() === null ? craft()->db->beginTransaction() : null;
+            try
+            {
+                // Fire an 'onBeforeSaveApplication' event
+                $this->onBeforeSaveApplication(new Event($this, array(
+                    'application'      => $application,
+                    'isNewApplication' => $isNewApplication
+                )));
 
-				if (craft()->elements->saveElement($application))
-				{
-					// Now that we have an element ID, save it on the other stuff
-					if ($isNewApplication)
-					{
-						$applicationRecord->id = $application->id;
-					}
+                if (craft()->elements->saveElement($application))
+                {
+                    // Now that we have an element ID, save it on the other stuff
+                    if ($isNewApplication)
+                    {
+                        $applicationRecord->id = $application->id;
+                    }
 
-					$applicationRecord->save(false);
+                    $applicationRecord->save(false);
 
-					// Fire an 'onSaveEvent' event
-					$this->onSaveApplication(new Event($this, array(
-						'application'      => $application,
-						'isNewApplication' => $isNewApplication
-					)));
+                    // Fire an 'onSaveEvent' event
+                    $this->onSaveApplication(new Event($this, array(
+                        'application'      => $application,
+                        'isNewApplication' => $isNewApplication
+                    )));
 
-					if ($transaction !== null)
-					{
-						$transaction->commit();
-					}
+                    if ($transaction !== null)
+                    {
+                        $transaction->commit();
+                    }
 
-					return true;
-				}
-			}
-			catch (\Exception $e)
-			{
-				if ($transaction !== null)
-				{
-					$transaction->rollback();
-				}
+                    return true;
+                }
+            }
+            catch (\Exception $e)
+            {
+                if ($transaction !== null)
+                {
+                    $transaction->rollback();
+                }
 
-				throw $e;
-			}
-		}
+                throw $e;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	// Events
+    // Events
 
-	/**
-	 * Fires an 'onBeforeSaveApplication' event.
-	 *
-	 * @param Event $event
-	 */
-	public function onBeforeSaveApplication(Event $event)
-	{
-		$this->raiseEvent('onBeforeSaveApplication', $event);
-	}
+    /**
+     * Fires an 'onBeforeSaveApplication' event.
+     *
+     * @param Event $event
+     */
+    public function onBeforeSaveApplication(Event $event)
+    {
+        $this->raiseEvent('onBeforeSaveApplication', $event);
+    }
 
-	/**
-	 * Fires an 'onSaveApplication' event.
-	 *
-	 * @param Event $event
-	 */
-	public function onSaveApplication(Event $event)
-	{
-		$this->raiseEvent('onSaveApplication', $event);
-	}
+    /**
+     * Fires an 'onSaveApplication' event.
+     *
+     * @param Event $event
+     */
+    public function onSaveApplication(Event $event)
+    {
+        $this->raiseEvent('onSaveApplication', $event);
+    }
 }
