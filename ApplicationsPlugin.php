9069ce823b78a920b23707c0d23e0a4e3e481797
Jason McCallister <themccallister@gmail.com>
2016-04-28 21:53:18 -0700

    Update some items

--- ApplicationsPlugin.php
+++ ApplicationsPlugin.php
@@ -1,121 +1,123 @@
-<?php namespace Craft;
+<?php
+
+namespace Craft;
 
 // include enums for custom statuses
 include(dirname(__FILE__) . '/enums/ApplicationStatus.php');
 
-class ApplicationsPlugin extends BasePlugin {
+class ApplicationsPlugin extends BasePlugin
+{
+    /**
+     * Return the plugin name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return Craft::t('Applications');
+    }
 
-	/**
-	 * Return the plugin name
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return Craft::t('Applications');
-	}
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '0.9.1';
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getVersion()
-	{
-		return '0.9.1';
-	}
+    /**
+     * Return the developer name
+     *
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return 'Jason McCallister';
+    }
 
-	/**
-	 * Return the developer name
-	 *
-	 * @return string
-	 */
-	public function getDeveloper()
-	{
-		return 'Jason McCallister';
-	}
+    /**
+     * Return the developer URL
+     *
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return 'http://jasonmccallister.co';
+    }
 
-	/**
-	 * Return the developer URL
-	 *
-	 * @return string
-	 */
-	public function getDeveloperUrl()
-	{
-		return 'http://jasonmccallister.co';
-	}
+    /**
+     * Tell Craft we have a cp section
+     *
+     * @return bool
+     */
+    public function hasCpSection()
+    {
+        return true;
+    }
 
-	/**
-	 * Tell Craft we have a cp section
-	 *
-	 * @return bool
-	 */
-	public function hasCpSection()
-	{
-		return true;
-	}
+    /**
+     * Define the plugin settings
+     *
+     * @return array
+     */
+    protected function defineSettings()
+    {
+        return array(
+            'notificationEmail' => array(
+                AttributeType::Email,
+                'required' => true
+            ),
+            'notificationSubject' => array(
+                AttributeType::String,
+                'required' => true,
+                'default' => "You have a new application on your website"
+            ),
+            'notificationMessage' => array(
+                AttributeType::String,
+                'required' => true,
+                'default' => "You have a new application submission on your website"
+            )
+        );
+    }
 
-	/**
-	 * Define the plugin settings
-	 *
-	 * @return array
-	 */
-	protected function defineSettings()
-	{
-		return array(
-			'notificationEmail'   => array(
-				AttributeType::Email,
-				'required' => true
-			),
-			'notificationSubject' => array(
-				AttributeType::String,
-				'required' => true,
-				'default'  => "You have a new application on your website"
-			),
-			'notificationMessage' => array(
-				AttributeType::String,
-				'required' => true,
-				'default'  => "You have a new application submission on your website"
-			)
-		);
-	}
+    /**
+     * Render the plugin settings HTML template
+     *
+     * @return string
+     */
+    public function getSettingsHtml()
+    {
+        return craft()->templates->render('applications/_settings', array(
+            'settings' => $this->getSettings()
+        ));
+    }
 
-	/**
-	 * Render the plugin settings HTML template
-	 *
-	 * @return string
-	 */
-	public function getSettingsHtml()
-	{
-		return craft()->templates->render('applications/_settings', array(
-			'settings' => $this->getSettings()
-		));
-	}
-
-	/**
-	 * Register CP routes, best practice to route to controllers
-	 *
-	 * @return array
-	 */
-	public function registerCpRoutes()
-	{
-		return array(
-			'applications/forms' => array(
-				'action' => 'applications/forms/index'
-			),
-			'applications/forms/new' => array(
-				'action' => 'applications/forms/edit'
-			),
-			'applications/forms/(?P<formId>\d+)'=> array(
-				'action' => 'applications/forms/edit'
-			),
-			'applications' => array(
-				'action' => 'applications/index'
-			),
-			'applications/(?P<formHandle>{handle})/new' => array(
-				'action' => 'applications/new'
-			),
-			'applications/(?P<formHandle>{handle})/(?P<applicationId>\d+)' => array(
-				'action' => 'applications/edit'
-			)
-		);
-	}
+    /**
+     * Register CP routes, best practice to route to controllers
+     *
+     * @return array
+     */
+    public function registerCpRoutes()
+    {
+        return array(
+            'applications/forms' => array(
+                'action' => 'applications/forms/index'
+            ),
+            'applications/forms/new' => array(
+                'action' => 'applications/forms/edit'
+            ),
+            'applications/forms/(?P<formId>\d+)' => array(
+                'action' => 'applications/forms/edit'
+            ),
+            'applications' => array(
+                'action' => 'applications/index'
+            ),
+            'applications/(?P<formHandle>{handle})/new' => array(
+                'action' => 'applications/new'
+            ),
+            'applications/(?P<formHandle>{handle})/(?P<applicationId>\d+)' => array(
+                'action' => 'applications/edit'
+            )
+        );
+    }
 }
--- controllers/Applications_FormsController.php
+++ controllers/Applications_FormsController.php
@@ -2,108 +2,107 @@
 
 class Applications_FormsController extends BaseController {
 
-	/**
-	 * Form index
-	 */
-	public function actionIndex()
-	{
-		$variables['forms'] = craft()->applications_forms->getAllForms();
+    /**
+     * Form index
+     */
+    public function actionIndex()
+    {
+        $variables['forms'] = craft()->applications_forms->getAllForms();
 
-		$this->renderTemplate('applications/forms', $variables);
-	}
+        $this->renderTemplate('applications/forms', $variables);
+    }
 
-	/**
-	 * Edit a form.
-	 *
-	 * @param array $variables
-	 * @throws HttpException
-	 * @throws Exception
-	 */
-	public function actionEdit(array $variables = array())
-	{
-		$variables['brandNewForm'] = false;
+    /**
+     * Edit a form.
+     *
+     * @param array $variables
+     * @throws HttpException
+     * @throws Exception
+     */
+    public function actionEdit(array $variables = array())
+    {
+        $variables['brandNewForm'] = false;
 
-		if (!empty($variables['formId'])) {
-			if (empty($variables['form'])) {
-				$variables['form'] = craft()->applications_forms->getFormById($variables['formId']);
+        if (!empty($variables['formId'])) {
+            if (empty($variables['form'])) {
+                $variables['form'] = craft()->applications_forms->getFormById($variables['formId']);
 
-				if (!$variables['form']) {
-					throw new HttpException(404);
-				}
-			}
+                if (!$variables['form']) {
+                    throw new HttpException(404);
+                }
+            }
 
-			$variables['title'] = $variables['form']->name;
-		}
-		else {
-			if (empty($variables['form'])) {
-				$variables['form'] = new Applications_FormModel();
-				$variables['brandNewForm'] = true;
-			}
+            $variables['title'] = $variables['form']->name;
+        } else {
+            if (empty($variables['form'])) {
+                $variables['form'] = new Applications_FormModel();
+                $variables['brandNewForm'] = true;
+            }
 
-			$variables['title'] = Craft::t('Create a new form');
-		}
+            $variables['title'] = Craft::t('Create a new form');
+        }
 
-		$variables['crumbs'] = array(
-			array(
-				'label' => Craft::t('Applications'),
-				'url'   => UrlHelper::getUrl('applications')
-			),
-			array(
-				'label' => Craft::t('Forms'),
-				'url'   => UrlHelper::getUrl('applications/forms')
-			),
-		);
+        $variables['crumbs'] = array(
+            array(
+                'label' => Craft::t('Applications'),
+                'url' => UrlHelper::getUrl('applications')
+            ),
+            array(
+                'label' => Craft::t('Forms'),
+                'url' => UrlHelper::getUrl('applications/forms')
+            ),
+        );
 
-		$this->renderTemplate('applications/forms/_edit', $variables);
-	}
+        $this->renderTemplate('applications/forms/_edit', $variables);
+    }
 
-	/**
-	 * Saves a form
-	 */
-	public function actionSaveForm()
-	{
-		$this->requirePostRequest();
+    /**
+     * Saves a form
+     */
+    public function actionSaveForm()
+    {
+        $this->requirePostRequest();
 
-		$form = new Applications_FormModel();
+        $form = new Applications_FormModel();
 
-		// Shared attributes
-		$form->id = craft()->request->getPost('formId');
-		$form->name = craft()->request->getPost('name');
-		$form->handle = craft()->request->getPost('handle');
+        // Shared attributes
+        $form->id = craft()->request->getPost('formId');
+        $form->name = craft()->request->getPost('name');
+        $form->handle = craft()->request->getPost('handle');
 
-		// Set the field layout
-		$fieldLayout = craft()->fields->assembleLayoutFromPost();
-		$fieldLayout->type = ElementType::Asset;
-		$form->setFieldLayout($fieldLayout);
+        // Set the field layout
+        $fieldLayout = craft()->fields->assembleLayoutFromPost();
+        $fieldLayout->type = ElementType::Asset;
+        $form->setFieldLayout($fieldLayout);
 
-		// Save it
-		if (craft()->applications_forms->saveForm($form)) {
-			craft()->userSession->setNotice(Craft::t('Form saved.'));
-			$this->redirectToPostedUrl($form);
-		}
-		else {
-			craft()->userSession->setError(Craft::t('Couldn’t save the form.'));
-		}
+        // Save it
+        if (craft()->applications_forms->saveForm($form)) {
+            craft()->userSession->setNotice(Craft::t('Form saved.'));
+            $this->redirectToPostedUrl($form);
+        } else {
+            craft()->userSession->setError(Craft::t('Couldn’t save the form.'));
+        }
 
-		// Send the form back to the template
-		craft()->urlManager->setRouteVariables(array(
-			'form' => $form
-		));
-	}
+        // Send the form back to the template
+        craft()->urlManager->setRouteVariables(array(
+            'form' => $form
+        ));
+    }
 
-	/**
-	 * Deletes a form.
-	 */
-	public function actionDeleteForm()
-	{
-		$this->requirePostRequest();
-		$this->requireAjaxRequest();
+    /**
+     * Deletes a form.
+     */
+    public function actionDeleteForm()
+    {
+        $this->requirePostRequest();
+        $this->requireAjaxRequest();
 
-		$formId = craft()->request->getRequiredPost('id');
+        $formId = craft()->request->getRequiredPost('id');
 
-		craft()->applications_forms->deleteFormById($formId);
-		$this->returnJson(array(
-			'success' => true
-		));
-	}
+        craft()->applications_forms->deleteFormById($formId);
+        
+        $this->returnJson(array(
+            'success' => true
+        ));
+    }
 }
